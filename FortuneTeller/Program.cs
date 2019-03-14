0aead240e678e7122ebb1d95dfd9d6485970d96d
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-03-14 14:58:30 -0500

    closer?

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -8,7 +8,7 @@ namespace NextBirthday
         {
             Console.WriteLine("When is your birthday?");
             string userBirthdayInput = Console.ReadLine();
-            userBirthdayInput = String.Format("{0:MM/dd}", userBirthdayInput);
+            //userBirthdayInput = String.Format("{0:MM/dd}", userBirthdayInput);
             //Console.WriteLine("Has your birthday already passed this year?");
             //string userQuestion = Console.ReadLine();
             
@@ -24,16 +24,17 @@ namespace NextBirthday
         {
 
             DateTime currentDate = DateTime.Now;
-            int currentYear = currentDate.Year;
-            
-            
-            DateTime nextBirthday = userBirthday.AddYears(1);
-            
-            
-            
-            //string daysBetween = countdown.TotalDays.ToString();
-            //int daysBetweenint = Convert.ToInt32(daysBetween);
+            //int currentDateInt = Convert.ToInt32(currentDate);
+            //int currentYear = currentDate.Year;
+            //int birthdayMonth = userBirthday.Month;
+            //int birthdayDay = userBirthday.Day;
 
+            DateTime nextBirthday = userBirthday;
+            if (currentDate >= userBirthday)
+            {
+                nextBirthday = userBirthday.AddYears(1);
+            }            
+              
             return nextBirthday.DayOfWeek.ToString();
         }
         // TODO: Create a method that takes the date someone was born and returns the name of the day on their next birthday.
